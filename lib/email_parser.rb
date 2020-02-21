205225d1e93e8be607031aaa03383a0ad057ac53
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-02-21 23:21:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,4 +4,17 @@
 # or whitespace (' ').
 
 
+class EmailAdressParser
+attr_accessor :emails 
 
+def initialize(emails)
+  @emails = emails
+end 
+
+def parse
+  puts splitEmail = emails.split.collect {|address| address.split(',')} 
+  returnArr = splitEmail.flatten.uniq
+  returnArr
+end
+
+end
